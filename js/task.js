3e7f08ea0ed4f7ecf4e6fe84694a22b126955665
Dmitriy-Jakovlev <dimas39690023@gmail.com>
2021-01-18 12:20:47 +0200

    task

--- js/task.js
+++ js/task.js
@@ -105,9 +105,12 @@ console.log(getNamesSortedByFriendsCount(users));
 // Задание 10
 // Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
 
-const getSortedUniqueSkills = users => {
-  // твой код
-};
-
+const getSortedUniqueSkills = users => 
+users
+  .map(user=>user.skills)
+  .reduce((allSkills, user) => [...allSkills, ...user], [])
+  .filter((elem, index, array) => array.indexOf(elem) === index)
+  .sort((b, a) => b.localeCompare(a, 'en'));
+ 
 console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
